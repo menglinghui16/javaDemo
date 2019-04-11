7eb58377895397b287bc6d2b3fb76358a689f25c
menglinghui16 <1551799812@qq.com>
2019-04-11 15:51:45 +0800

    Update TestGit.java

--- src/main/java/git/test/TestGit.java
+++ src/main/java/git/test/TestGit.java
@@ -5,7 +5,7 @@ package git.test;
  */
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println("commit1-冲突1-测试冲突2");
+        System.out.println("commit1-冲突22");
         System.out.println("commit2");
         System.out.println("commit3");
     }
