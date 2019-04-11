4398bc5e215ce47e23f7204aca4e45ecf5ac506e
menglinghui <1551799812@qq.com>
2019-04-11 15:39:24 +0800

    git提交测试-多次commit1

---
+++ src/main/java/git/test/TestGit.java
@@ -0,0 +1,10 @@
+package git.test;
+
+/**
+ * Created by Administrator on 2019/4/11.
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("commit1");
+    }
+}
