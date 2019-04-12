0030299c9ce8fe270a8c6c733bc568f29a2fca1d
menglinghui <1551799812@qq.com>
2019-04-12 09:34:55 +0800

    测试新的分支

---
+++ src/main/java/git/test/TestBranch.java
@@ -0,0 +1,10 @@
+package git.test;
+
+/**
+ * Created by Administrator on 2019/4/12.
+ */
+public class TestBranch {
+    public static void main(String[] args) {
+        System.out.println("测试新的分支branch使用");
+    }
+}
