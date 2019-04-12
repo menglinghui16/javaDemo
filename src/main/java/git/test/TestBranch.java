612eb1d8de5b8d224a9d4b337e669a2f899328ba
menglinghui <1551799812@qq.com>
2019-04-12 10:39:57 +0800

    11

--- src/main/java/git/test/TestBranch.java
+++ src/main/java/git/test/TestBranch.java
@@ -7,6 +7,8 @@ public class TestBranch {
     public static void main(String[] args) {
         System.out.println("测试新的分支branch使用");
         System.out.println("dev分支使用学习-创建");
-        System.out.println("在idea中禁用（--no-ff），fast forward模式，以便获取分支信息，右击项目，git-merger选择");
+        System.out.println("在idea中禁用 （--no-ff），fast forward模式，以便获取分支信息，右击项目，git-merger选择");
+        System.out.println("stash如何使用");
+
     }
 }
