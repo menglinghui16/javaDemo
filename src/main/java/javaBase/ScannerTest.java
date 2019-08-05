9f9963fde536d294f3aab27207de5c4a09085dde
menglinghui <1551799812@qq.com>
2019-08-05 10:36:34 +0800

    Scanner类方法

--- src/main/java/javaBase/ScannerTest.java
+++ src/main/java/javaBase/ScannerTest.java
@@ -7,12 +7,17 @@ import java.util.Scanner;
  */
 public class ScannerTest {
     public static void main(String[] args) {
-        //导包java.util.Scanner；
-        //创建Scanner对象
-        Scanner input = new Scanner(System.in);
-        //获取控制台数据
-        String str = input.next();
-        int res = input.nextInt();
-        Double dou = input.nextDouble();
+//        //导包java.util.Scanner；
+//        //创建Scanner对象
+//        Scanner input = new Scanner(System.in);
+//        //获取控制台数据
+//        String str = input.next();
+//        int res = input.nextInt();
+//        Double dou = input.nextDouble();
+
+        Scanner sc = new Scanner(System.in);//System.in代表标准输入，就是键盘输入
+        while (sc.hasNext()){
+            System.out.println("键盘输入的内容是："+sc.next());
+        }
     }
 }
