bf9c19fe38a95c9a304ecd48ada65ce195914a98
menglinghui <1551799812@qq.com>
2019-09-26 17:48:33 +0800

    commit002

---
+++ src/main/java/git/test/TestGitPush.java
@@ -0,0 +1,10 @@
+package git.test;
+
+/**
+ * Created by Administrator on 2019/9/26.
+ */
+public class TestGitPush {
+    public static void main(String[] args) {
+        System.out.println("commit002");
+    }
+}
