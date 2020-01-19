e995673b0f01761ccd8b72e20d031a45af2a19bf
menglinghui <1551799812@qq.com>
2020-01-19 16:50:50 +0800

    merge into current 分支合并测试

--- src/main/java/git/test/TestBranch.java
+++ src/main/java/git/test/TestBranch.java
@@ -7,5 +7,9 @@ public class TestBranch {
     public static void main(String[] args) {
         System.out.println("测试新的分支branch使用");
         System.out.println("dev分支使用学习-创建");
+        System.out.println("本地新建一个分支，未提交到远程的分支，测试合并到本地主分支master");
+        System.out.println("test");
+        System.out.println("在Git:test分支，选择local的master分支，然后merge into current," +
+                "结果为将当前test分支修改的内容，合并到master分支上去--可查看图片");
     }
 }
