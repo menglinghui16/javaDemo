7876b08dc8dd62311a764d7be3e7243dde9b8ce4
menglinghui <1551799812@qq.com>
2020-07-28 15:10:53 +0800

    测试rebase current onto selected

--- src/main/java/git/test/TestGitPush.java
+++ src/main/java/git/test/TestGitPush.java
@@ -7,5 +7,6 @@ public class TestGitPush {
     public static void main(String[] args) {
         System.out.println("commit002");
         System.out.println("测试提交代码");
+        System.out.println("测试一下rebase current onto selected");
     }
 }
