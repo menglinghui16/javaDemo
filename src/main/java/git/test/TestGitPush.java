8095383953d205d3dfe2b63374cefecc2f279dc0
menglinghui16 <1551799812@qq.com>
2020-07-28 18:50:09 +0800

    Update TestGitPush.java

--- src/main/java/git/test/TestGitPush.java
+++ src/main/java/git/test/TestGitPush.java
@@ -9,5 +9,6 @@ public class TestGitPush {
         System.out.println("测试提交代码");
         System.out.println("测试一下rebase current onto selected");
         System.out.println("dev分支拉去master分支上的最新的代码");
+        System.out.println("在github上修改master分支内容，让dev分支拉取master分支上的最新的代码");
     }
 }
