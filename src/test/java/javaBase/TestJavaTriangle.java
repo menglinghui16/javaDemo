9e516da28486a766f7714c5a1b8581a0241b6f95
menglinghui <1551799812@qq.com>
2020-11-13 16:45:22 +0800

    idea开发工具使用junit必须要创建test-java文件件，并设置为tests，才能引入@Test注解使用单元测试

---
+++ src/test/java/javaBase/TestJavaTriangle.java
@@ -0,0 +1,19 @@
+package javaBase;
+
+import org.junit.Test;
+
+/**
+ * @Author: menglh
+ * @Description:
+ * @Date: 2020/11/13 16:43
+ */
+public class TestJavaTriangle {
+    @Test
+    public void testWhile(){
+        int count = 1;
+        while (count <= 5){
+            System.out.println("hello world "+ count);
+            count++;
+        }
+    }
+}
